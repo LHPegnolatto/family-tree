6f36e6402546186125e7a84a7b17131380f0d4ff
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2021-03-04 20:08:00 -0300

    Added react-icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import { Button } from '@chakra-ui/button';
+import { IconButton } from '@chakra-ui/button';
 import { useColorMode } from '@chakra-ui/color-mode';
-import { Center } from '@chakra-ui/layout';
+import { Flex } from '@chakra-ui/layout';
+import { useToast } from '@chakra-ui/toast';
 import Head from 'next/head';
+import { FiMoon, FiSun } from 'react-icons/fi';
 
 export default function Home() {
-  const { toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const toast = useToast();
 
   return (
     <div>
@@ -13,9 +16,26 @@ export default function Home() {
       </Head>
 
       <main>
-        <Center h="100vh">
-          <Button onClick={toggleColorMode}>Toggle theme</Button>
-        </Center>
+        <Flex h="100%" align="flex-start" justify="flex-end" m={2}>
+          <IconButton
+            aria-label="Search database"
+            icon={colorMode === 'light' ? <FiMoon /> : <FiSun />}
+            onClick={() => {
+              toggleColorMode();
+
+              toast.closeAll();
+              toast({
+                position: 'bottom-left',
+                title: 'Tema alterado.',
+                description: `O tema foi alterado com sucesso para o modo ${
+                  colorMode === 'light' ? 'escuro' : 'claro'
+                }.`,
+                status: 'info',
+                duration: 2000,
+              });
+            }}
+          />
+        </Flex>
       </main>
     </div>
   );
